b4e6ae9f10fcba35419ac522234605198a36349c
Sakib1994 <mdnazmussakibcse@gmail.com>
2023-07-07 09:51:33 +0600

    init

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,49 @@
+import { HttpStatus, Injectable, NotFoundException} from '@nestjs/common';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const newCoffee = { id: this.coffees.length + 1, ...createCoffeeDto };
+    this.coffees.push(newCoffee);
+    return newCoffee;
+  }
+
+  findAll() {
+    // findAll(limit, offset) {
+    return this.coffees;
+  }
+
+  findOne(id: number) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      return 'May be updated';
+    }
+  }
+
+  remove(id: number) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+    return `This action removes a #${id} coffee`;
+  }
+}
